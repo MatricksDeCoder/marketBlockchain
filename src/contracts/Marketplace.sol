092fb66a4164e8b141f8e2527c3df623b21741c3
MatricksDeCoder <infinitumcubed@gmail.com>
2019-08-22 13:20:13 +0200

    allow for purchasing product and associated tests

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -10,19 +10,51 @@ contract Marketplace {
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
+
+    event ProductCreated(  
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+        );
+
+    event ProductPurchased(  
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+        );
 
     constructor() public {
         name = "Marketplace";
     }
 
-    function createProduct() public {
-        //product = new Product()
-        //product.purchased = false;
-        //validate product is correct
-        //create product
-        //trigger and event 
+    function createProduct(string memory _name, uint _price) public {
+        require(bytes(_name).length>0);
+        require(_price > 0);
+        productCount++;
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    function purchaseProduct(uint _id) public payable {
+        require(_id > 0 && _id <= productCount);
+        Product memory _product = products[_id]; //copy of product in blockchain storage
+        address payable _seller = _product.owner;
+        string memory _name     = _product.name;
+
+        //Change ownership
+        _product.owner  = msg.sender;
+        _product.purchased = true;
+        //Update product in storage
+        products[_id] = _product;
+        //Transfer funds to previous owner
+        address(_seller).transfer(msg.value);
+        emit ProductPurchased(productCount,_name,msg.value, msg.sender, true);
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,8 +1,14 @@
 const Marketplace = artifacts.require('Marketplace.sol');
+require('chai').use(require('chai-as-promised'))
+               .should();
 
 //Implement test within here
 contract('Marketplace', (accounts) => {
     let marketplaceContract;
+    let deployer = accounts[0];
+    let seller1   = accounts[1];
+    let buyer1   = accounts[2];
+    let seller2  = accounts[3];
 
     beforeEach(async ()=> {
         marketplaceContract = await Marketplace.deployed();
@@ -26,17 +32,75 @@ contract('Marketplace', (accounts) => {
         })
     });
 
-    describe('product creation', async () => {
+    describe('products', async () => {
 
-        let product, productCount;
+        let product, productCount, productName;
+        let prod1_name  = 'Androidbox';
+        let prod1_price = web3.utils.toWei('1', 'ether');
+        let prod2_name = 'Applebox';
+        let prod2_price = web3.utils.toWei('3', 'ether');
+
         beforeEach(async ()=>{
-            product = await marketplaceContract.createProduct();
+            product = await marketplaceContract.createProduct(prod1_name, prod1_price, {from: seller1});
             productCount = await marketplaceContract.productCount();
         });
 
-        it('changes productCount', async () => {
-
+        it('creates product', async () => {
+            assert.equal(productCount,1, 'Product Count is Correct!');
+            const event = product.logs[0].args;
+            assert.equal(event.id.toNumber(),productCount.toNumber(),'Product id is Correct!');
+            assert.equal(event.name,prod1_name, 'Product Name is Correct!');
+            assert.equal(event.price,prod1_price,'Product Price is Correct!');
+            assert.equal(event.owner,seller1, 'Owner address is Correct!');
+            assert.equal(event.purchased, false,'Product is not yet Purchased!');
         });
+
+        it('rejects product with no name or price not right', async() => {
+               //FAILURES:
+            //Product must have a name 
+            await marketplaceContract.createProduct('',prod1_price,{from:seller1}).should
+                                                                                          .be
+                                                                                          .rejected;
+            //Product no price must be rejected
+            await marketplaceContract.createProduct(prod1_name,0,{from:seller1}).should 
+                                                                                          .be
+                                                                                          .rejected;
+        });
+
+        it('lists products', async () => {
+
+            const product = await marketplaceContract.products(productCount);
+            assert.equal(product.id,productCount.toNumber(),'Product id is Correct!');
+            assert.equal(product.name,prod1_name, 'Product Name is Correct!');
+            assert.equal(product.price,prod1_price,'Product Price is Correct!');
+            assert.equal(product.owner,seller1, 'Owner address is Correct!');
+            assert.equal(product.purchased, false,'Product is not yet Purchased!');
+        });
+
+        it('sells product', async () => {
+
+            //track sellers balance before purchase 
+            let seller1FundsBefore = await web3.eth.getBalance(seller1);
+            seller1FundsBefore = new web3.utils.BN(seller1FundsBefore);
+
+            const sale = await marketplaceContract.purchaseProduct(productCount,{from:buyer1,value:prod1_price});
+            //check logs are correct
+            const event = sale.logs[0].args;
+            assert.equal(event.id.toNumber(),productCount.toNumber(),'Product id is Correct!');
+            assert.equal(event.name,prod1_name, 'Product Name is Correct!');
+            assert.equal(event.price,prod1_price,'Product Price is Correct!');
+            assert.equal(event.owner,buyer1, 'New Owner is Buyer, address is Correct!');
+            assert.equal(event.purchased, true,'Product is Purchased!');
+
+            //check that seller receives funds
+            let seller1FundsAfter = await web3.eth.getBalance(seller1);
+            seller1FundsAfter     = new web3.utils.BN(seller1FundsAfter);
+            let price = new web3.utils.BN(prod1_price);
+            assert.equal(seller1FundsAfter.toString(),seller1FundsBefore.add(price).toString());
+        });
+
+       
+
     });
 
 });
